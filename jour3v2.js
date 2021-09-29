65bca0adae18a735cf63330acaddd2208e0a0d23
kevinalbert7 <20ko@live.fr>
2021-09-29 18:39:06 +0200

    jour3v2

--- jour3v2.js
+++ jour3v2.js
@@ -1,11 +1,70 @@
-// 01
+// 01 Nombre pairs
 
-var i = 11
-var limit = 47
+var min = 11
+var max = 47
+var sum = 0
+
+for(var i = 11; i < 47; i++){
+    var even = i % 2 === 0
+    if(even){
+        console.log(i)
+        sum = sum + i
+    }
+}
+
+console.log(sum)
+
+// 02 Puissance et racine carrée
+
+var min = 109
+var max = 588
+var sum = 0
+
+for(var i = 109; i < 588; i++){
+    var odd = i % 2 === 1
+    if(odd){
+        console.log(i)
+        sum = sum + i
+    }
+}
+console.log(sum)
+
+// 03 Puissance et racine carrée
+
+var numbers = [1, 2, 3, 4, 5]
+var squares = []
+var roots = []
+
+for( var i = 0; i < numbers.length; i++){
+    console.log(i)
+    var puissance = numbers[i] * numbers[i]
+    squares.push(puissance) 
+}
+console.log(squares)
+
+for(var i = 0; i < squares.lenght; i++){
+    console.log(i)
+    var racine = Math.sqrt(squares[i])
+    roots.push(racine)
+}
+console.log(roots)
 
 
-for( i = 11; i <= 47; i++){
-var even = i % 2 == 0
+// 04 Table de multiplication v1
 
-    console.log(even(i))
+max = 30
+
+for(var i = 0 ; i < max; i++){
+    var multiple = i % 3 == 0
+    if(multiple){
+        console.log(i)
+    }
+}
+
+// 5  Table de multiplication v2
+
+for( var i = 0; i < 11; i++){
+    console.log(i)
+    for(var j = 0; j < 11; j++){;;
+    }
 }
